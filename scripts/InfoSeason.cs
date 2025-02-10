9210ac017453c50d1000d1da854e0e43a45b1764
Gabriel Gagnon <gabriel.gagnon97@gmail.com>
2025-02-10 00:47:49 -0500

    v1.5 nbre episode per season started

---
+++ scripts/InfoSeason.cs
@@ -0,0 +1,16 @@
+using Godot;
+using System;
+
+public partial class InfoSeason : Control
+{
+	public override void _Ready()
+	{
+	}
+
+	public int[] GetNbrEpiSeason()
+	{
+		int[] toReturn = Array.Empty<int>();
+		
+		return toReturn;
+	}
+}
